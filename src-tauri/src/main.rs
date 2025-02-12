cea0d4b5f55d245d54edd317b50d39538a8e4720
clemence <cchopin@altima-assurances.fr>
2025-02-12 18:17:11 +0100

    Initial commit with updated .gitignore

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use std::process::Command;
+
+#[tauri::command]
+fn open_link(link: &str) -> Result<(), String> {
+  println!("open_link appelé avec le lien : {}", link);
+  let status = Command::new("/usr/bin/open")
+      .args(&["-a", "Mumble", link])
+      .status()
+      .map_err(|e| e.to_string())?;
+  println!("Statut de la commande open : {:?}", status);
+  if !status.success() {
+      return Err(format!("La commande open a retourné un code non nul: {:?}", status));
+  }
+  Ok(())
+}
+
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![open_link])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
