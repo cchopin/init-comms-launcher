a2ca9a691fe0d16618b7c8503a0e857bb2d871f6
Seb-Dz <sebastienduruz@protonmail.com>
2025-02-12 19:56:28 +0100

    Add a specific call for opening links on Windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,14 +8,31 @@ use std::process::Command;
 #[tauri::command]
 fn open_link(link: &str) -> Result<(), String> {
   println!("open_link appelé avec le lien : {}", link);
+
+  // call for windows
+  #[cfg(target_os = "windows")]
+  let status = Command::new("cmd")
+        .args(&["/C", "start", "", link])
+        .status()
+        .map_err(|e| e.to_string())?;
+
+  println!("Statut de la commande start : {:?}", status);
+  if !status.success() {
+      return Err(format!("La commande open a retourné un code non nul: {:?}", status));
+  }
+
+  // call for linux/macos
+  #[cfg(target_os = "macos")]
   let status = Command::new("/usr/bin/open")
       .args(&["-a", "Mumble", link])
       .status()
       .map_err(|e| e.to_string())?;
+
   println!("Statut de la commande open : {:?}", status);
   if !status.success() {
       return Err(format!("La commande open a retourné un code non nul: {:?}", status));
   }
+
   Ok(())
 }
 
